5c37ce40b57e1c82d80a14d3381c048d38bc06b2
miftahulacil0608 <miftahulacil0608@gmail.com>
2024-06-03 19:16:25 +0700

    add landing page

--- Capstone-React-6/src/App.jsx
+++ Capstone-React-6/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Homepage from './pages/Home/Homepage'
+// import "./App.css";
+import React from "react";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-
   return (
     <>
-      <div>Home</div>
-      <Homepage />
+      {/* <div>Home</div> */}
+      <div>
+        <LandingPage />
+      </div>
+      {/* <Homepage /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
