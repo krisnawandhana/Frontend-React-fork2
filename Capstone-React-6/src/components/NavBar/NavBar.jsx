856ba5104a4d4e4661f4ae0e746198e2139d3fec
raraisahh <111560607+raraisahh@users.noreply.github.com>
2024-06-04 22:16:55 +0700

    Update Navbar

--- Capstone-React-6/src/components/NavBar/NavBar.jsx
+++ Capstone-React-6/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import logo from "../../assets/img/Logo-img.png";
+import React from 'react';
 
-const Navbar = () => {
+export default function NavBar() {
   return (
     <div>
       <nav className="bg-[#F1F3F6] border-gray-200">
-        <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+        <div className="w-11/12 flex items-center justify-between mx-auto p-4">
+          {/* Logo Mindease */}
+          <div className="flex items-center justify-end">
+            <a href="#" className="flex items-center space-x-3 ">
+              <img src="logo/mindease-logo.svg" className="h-8" alt="Mindease Logo" />
+            </a>
+          </div>
+
           {/* Daftar Menu dan Tombol Menu Responsive */}
           <div className="flex items-center space-x-3 md:justify-start w-full md:w-auto">
             <button
@@ -16,83 +22,34 @@ const Navbar = () => {
               aria-expanded="false"
             >
               <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
               </svg>
             </button>
 
             {/* Daftar Menu */}
-            <div
-              className="hidden w-full md:flex md:w-auto"
-              id="navbar-default"
-            >
+            <div className="hidden w-full md:flex md:w-auto" id="navbar-default">
               <ul className="text-body1 font-regular flex flex-col md:flex-row mt-4 md:mt-0">
                 <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary"
-                  >
-                    Home
-                  </a>
+                  <a href="#" className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary">Home</a>
                 </li>
                 <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary"
-                  >
-                    Our Services
-                  </a>
+                  <a href="#" className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary">Our Services</a>
                 </li>
                 <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary"
-                  >
-                    About Us
-                  </a>
+                  <a href="#" className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary">About Us</a>
                 </li>
                 <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary"
-                  >
-                    Community
-                  </a>
+                  <a href="#" className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary">Community</a>
                 </li>
                 <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary"
-                  >
-                    Contact
-                  </a>
+                  <a href="#" className="block py-2 px-3 text-dark-3 border-b-2 border-transparent hover:bg-transparent hover:text-primary hover:border-b-2 hover:border-primary">Contact</a>
                 </li>
               </ul>
             </div>
-          </div>
-
-          {/* Logo Mindease */}
-          <div className="flex items-center justify-end">
-            <a href="/" className="flex items-center space-x-3 ">
-              <img src={logo} className="" alt="Mindease Logo" />
-            </a>
           </div>
         </div>
       </nav>
     </div>
   );
-};
-
-export default Navbar;
+}
