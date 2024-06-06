9d50fce392295869be0587962b658032a02e35b9
raraisahh <111560607+raraisahh@users.noreply.github.com>
2024-06-06 23:06:43 +0700

    dashboard routing

--- Capstone-React-6/src/App.jsx
+++ Capstone-React-6/src/App.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { isAuthenticated } from './utils/auth';
 import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import Homepage from './pages/Home/Homepage';
+import LandingPage from './pages/LandingPage/LandingPage';
+import DashboardLayout from './layouts/DashboardLayout';
+import Dashboard from './pages/Dashboard/Dashboard';
 import ManageContent from './pages/ManageContent/ManageContent';
 import ManagePatient from './pages/ManagePatient/ManagePatient';
 import Transaction from './pages/Transaction/Transaction';
-import LandingPage from './pages/LandingPage/LandingPage';
-import Register from './pages/Register/Register';
-import Dashboard from './pages/Dashboard/Dashboard';
+import ManageForum from './pages/ManageForum/ManageForum';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -23,11 +25,14 @@ function App() {
       <Route path="/" element={isLoggedIn ? <Navigate to="/homepage" /> : <Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/homepage" element={<Homepage />} />
-      <Route path="/managecontent" element={<ManageContent />} />
-      <Route path="/managepatient" element={<ManagePatient />} />
-      <Route path="/transaction" element={<Transaction />} />
       <Route path="/landingpage" element={<LandingPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<DashboardLayout />} >
+        <Route index element={<Dashboard />} />
+        <Route path="managecontent" element={<ManageContent />} />
+        <Route path="managepatient" element={<ManagePatient />} />
+        <Route path="transaction" element={<Transaction />} />
+        <Route path="manageforum" element={<ManageForum />} />
+      </Route>
     </Routes>
   );
 }
