6671031f29554614dd8c57817e2ed673bf5b3076
Krisna Wandhana <warriorkrisna@gmail.com>
2024-06-11 18:19:38 +0800

    update dynamic for avatar

--- Capstone-React-6/src/components/Transaction/NewestCard.jsx
+++ Capstone-React-6/src/components/Transaction/NewestCard.jsx
@@ -1,19 +1,20 @@
-const NewestCard = ({ variant, detail, count, date, name, id }) => {
+const NewestCard = ({ variant, detail, count, date, name, id, avatar }) => {
 	const variants = {
 		sukses: {
-			icon: "/avatar/avatar.jpg",
+
             color: "bg-success",
 		},
         gagal: {
-			icon: "/avatar/avatar.jpg",
             color: "bg-error",
 		},
 	}
+	const nominalAmount = parseFloat(count);
+
 	return (
 		<div className="rounded-full w-full h-full overflow-hidden flex justify-start items-center bg-white outline outline-2 outline-dark-4" >
 			<div className="flex flex-row gap-x-2 items-center justify-center">
 				<div className="h-16 aspect-square flex justify-center items-center">
-					<img src={variants[variant].icon} className="p-2 rounded-full" />
+					<img src={`../../../public/Dashboard/${avatar}`} alt={avatar} className="p-2 rounded-full" />
 				</div>
 				<div className="gap-y-2 text-dark-2 font-semibold text-center text-base">
 					<p className="text-start">{name}</p>
@@ -21,8 +22,8 @@ const NewestCard = ({ variant, detail, count, date, name, id }) => {
 						<p className="text-body1">{id}</p>
 					</div>
 				</div>
-                <div className="flex flex-row gap-x-8 ml-8 align-middle items-center text-dark-2 text-body1 font-semibold">
-                    <p className="text-center">RP{count}</p>
+                <div className="flex flex-row gap-x-6 mx-auto ml-3 align-middle items-center text-dark-2 text-body1 font-semibold">
+                    <p className="text-center">{nominalAmount.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</p>
                     <p className="text-center">{date}</p>
                     <p className={`text-white p-2 rounded-full w-24 text-center uppercase ${variants[variant].color}`}>{detail}</p>
                 </div>
